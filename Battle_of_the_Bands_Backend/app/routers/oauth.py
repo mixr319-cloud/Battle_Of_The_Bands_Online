7952254c00a9a52a4d1317f4bad1569ce4e166bb
mixr319-cloud <mixr319@gmail.com>
2026-06-21 12:46:08 -0500

    Update oauth.py

--- Battle_of_the_Bands_Backend/app/routers/oauth.py
+++ Battle_of_the_Bands_Backend/app/routers/oauth.py
@@ -28,9 +28,9 @@ DISCORD_CLIENT_ID     = os.getenv("DISCORD_CLIENT_ID", "")
 DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET", "")
 DISCORD_REDIRECT_URI  = os.getenv("DISCORD_REDIRECT_URI", "http://localhost:8000/auth/discord/callback")
 
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
-GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/google/callback")
+GOOGLE_CLIENT_ID      = os.getenv("GOOGLE_CLIENT_ID", "")
+GOOGLE_CLIENT_SECRET  = os.getenv("GOOGLE_CLIENT_SECRET", "")
+GOOGLE_REDIRECT_URI   = os.getenv("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/google/callback")
 
 # After OAuth we redirect the user back to the frontend with their profile encoded
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
